dafe8de99dc00380afae3a0f7b9bb5f95e85c5f4
nmy0502 <minyeong.nam@jsct.co.kr>
2026-08-26 15:12:28 +0900

    점선 추가

--- js/praise.js
+++ js/praise.js
@@ -1,10 +1,10 @@
 // const SHEET_ID = "1-ILVOg2DyAmnuE127iSaUnnDcmbrpjjgcoRTs0vOTf0";
 const SHEET_ID = "1LqUQ0cEDyys8JDrWDXfm7u33d7IAfMChdW7vksJ-i2U";
+let currentDate = new Date(); // 오늘 날짜로 시작
 
-const TEST_DATE = new Date(2026, 7, 29); // 2026년 8월 29일
+const TEST_DATE = new Date(2026, 7, 29); // 테스트 날짜 (m+1)월
+// currentDate = TEST_DATE;
 
-let currentDate = TEST_DATE; // 테스트 날짜
-// let currentDate = new Date(); // 오늘 날짜로 시작
 currentDate.setHours(0, 0, 0, 0); // 시간 초기화
 let allData = [];
 
