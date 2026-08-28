4f63441be4bc073c18e1005c589dd0cb4f8eedd6
nmy0502 <minyeong.nam@jsct.co.kr>
2026-08-28 16:13:07 +0900

    index페이지 통로로 수정, 오카리나 페이지파일 생성

---
+++ js/ocarina.js
@@ -0,0 +1,17 @@
+function formatKoreanDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}년 ${month}월 ${day}일`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const today = new Date();
+
+  // 오늘 날짜
+  document.getElementById("today-date").textContent = formatKoreanDate(today);
+
+  // 현재 월
+  document.getElementById("this-month").textContent = `${today.getMonth() + 1}월`;
+});
